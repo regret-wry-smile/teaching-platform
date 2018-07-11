4cf16c5e686531c4db89c66ded0137f3bb25db92
shenyijie <shenyijie@huixiangtech.cn>
2018-07-11 17:25:11 +0800

    新增线程管理

---
+++ src/main/java/com/zkxltech/thread/ThreadManager.java
@@ -0,0 +1,43 @@
+package com.zkxltech.thread;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author ShenYijie
+ * @Description: TODO( 线程管理 )
+ * @date 2018年7月11日
+ * 
+ */
+public class ThreadManager {
+	private static final Logger logger = LoggerFactory.getLogger(ThreadManager.class);
+	private static ThreadManager instance = null;
+	private static List<Thread> threads = Collections.synchronizedList(new ArrayList<>());
+	private ThreadManager() {
+		
+	}
+	public ThreadManager getInstance() {
+		if(instance==null) {
+			instance = new ThreadManager();
+		}
+		return instance;
+	}
+	
+	public synchronized void addThread(Thread thread) {
+		threads.add(thread);
+	}
+	
+	public synchronized static void removeThread(Thread thread) {
+		threads.remove(thread);
+	}
+	
+	public synchronized static void printThead() {
+		logger.info("当前线程个数:" + threads.size());
+	}
+	
+	
+}
